c3277b77cc653b8f4f7d4e252f29cf7b7132ae54
yoppedayo8545 <yoppedayo8545@gmail.com>
2021-03-02 14:54:21 +0900

    Issue4

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -23,9 +23,6 @@ class CalendarsController < ApplicationController
 
     @wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
 
-  def get_week
-    wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
-
 
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
     @todays_date = Date.today
